b92bfa93e4a83b4c3b991205ef9c19e3d7220885
Salma <salma--95@hotmail.com>
2020-10-12 22:47:19 +0300

    Updates

---
+++ precache-manifest.6ee141b472ef590907326a5f556318b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5029dc309a8aef4dcb078d571122559",
+    "url": "/memory-cards-react-game/index.html"
+  },
+  {
+    "revision": "ca68a968625ee863c608",
+    "url": "/memory-cards-react-game/static/css/main.4e31eefd.chunk.css"
+  },
+  {
+    "revision": "2402d2b6de00252b83e8",
+    "url": "/memory-cards-react-game/static/js/2.c6de1266.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-cards-react-game/static/js/2.c6de1266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca68a968625ee863c608",
+    "url": "/memory-cards-react-game/static/js/main.dde16355.chunk.js"
+  },
+  {
+    "revision": "7187848636f1ee319d28",
+    "url": "/memory-cards-react-game/static/js/runtime-main.4a792488.js"
+  }
+]);
